a3acbb82bc43d02981ed2c24e5f3185c99038478
Jardel <jardelgalvao1@gmail.com>
2024-05-17 20:56:29 -0300

    Update angular core

--- my-little-world/src/app/components/registration-page/registration-page.component.ts
+++ my-little-world/src/app/components/registration-page/registration-page.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
   styleUrls: ['./registration-page.component.css']
 })
+
 export class RegistrationPageComponent {
   form: FormGroup;
   
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
   onSubmit(): void {
     if (this.form.valid) {
       console.log('Form Submitted!', this.form.value);
@@ -15,4 +26,6 @@ export class RegistrationPageComponent {
     } else {
       console.log('Form is invalid');
     }
+
+  }  
 }
